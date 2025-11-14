0a62f3b0b9186440e150975aaa7d41893a897f26
meikpiep <meik.public@piepmeyer.org>
2025-11-14 19:54:52 +0100

    Upgrade Android SDK version from 35 to 36

--- buildSrc/src/main/kotlin/ru/github/igla/ferriswheel/Configuration.kt
+++ buildSrc/src/main/kotlin/ru/github/igla/ferriswheel/Configuration.kt
@@ -1,9 +1,9 @@
 package ru.github.igla.ferriswheel
 
 object Configuration {
-    const val COMPILE_SDK = "android-35"
-    const val BUILD_TOOLS = "35.0.0"
+    const val COMPILE_SDK = "android-36"
+    const val BUILD_TOOLS = "36.0.0"
     const val MIN_SDK = 15
-    const val TARGET_SDK = 35
+    const val TARGET_SDK = 36
     const val KOTLIN_VERSION = "2.2.21"
 }
